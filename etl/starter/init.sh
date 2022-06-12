bad5203ae74fd2e6b04ff772b695693a57dabff8
specnazm <laketic.milena98@gmail.com>
2022-06-13 01:32:17 +0200

    test: added test command to init.sh

--- etl/starter/init.sh
+++ etl/starter/init.sh
@@ -1,3 +1,4 @@
 pip3 install requests
 cd home/app
 /spark/bin/spark-submit  --driver-class-path /home/app/postgresql-42.4.0.jar  jobs/etl_job.py
+/spark/bin/spark-submit  --driver-class-path /home/app/postgresql-42.4.0.jar  tests/test.py
